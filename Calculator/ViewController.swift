1ea0088e8f7e3ad2114a56f731738302d88748c5
Adriano Rodrigues <adrikei@gmail.com>
2015-03-29 03:10:09 -0300

    Main functionality and UI
    added most basic operations I wanted it to have

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+	@IBOutlet weak var display: UILabel!
+
+	var userIsInTheMiddleOfTypingANumber = false
+	var firstValue: Double!
+	var op: String!
+	
+	var displayValue: Double {
+		get{
+			return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+		}
+		set{
+			display.text = "\(newValue)"
+		}
 	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	
+	@IBAction func appendDigit(sender: UIButton) {
+		let digit = sender.currentTitle!
+		if userIsInTheMiddleOfTypingANumber {
+			display.text = display.text! + digit
+		}else{
+			display.text = digit
+			userIsInTheMiddleOfTypingANumber = true
+		}
 	}
-
-
+	
+	@IBAction func equals() {
+		userIsInTheMiddleOfTypingANumber = false
+		var secondValue = displayValue
+		switch op {
+			case "÷": displayValue = self.firstValue / secondValue
+			case "×": displayValue = self.firstValue * secondValue
+			case "−": displayValue = self.firstValue - secondValue
+			case "+": displayValue = self.firstValue + secondValue
+			default: break
+		}
+	}
+	
+	func resetDisplay() {
+		display.text = "0"
+		userIsInTheMiddleOfTypingANumber = false
+	}
+	
+	@IBAction func operate(sender: UIButton) {
+		firstValue = displayValue
+		resetDisplay()
+		op = sender.currentTitle!
+	}
+	
 }
 
